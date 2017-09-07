e89d952d418b173878826c3a7afd7336f2bec61c
Ahmed <eng.ahmed_gamal@yahoo.com>
2017-09-07 03:13:36 +0200

    Layout

--- resources/views/instagram/componant/image.blade.php
+++ resources/views/instagram/componant/image.blade.php
@@ -1,13 +1,11 @@
-<form method="post" action="{{url('download')}}" target="_blank">
-    {{csrf_field()}}
-    <input type="hidden" name="url" value="{{$item['standard_resolution']['url']}}">
-    <figure class="figure col-md-4">
-        <img style="height: 400px !important;" src="{{$item['low_resolution']['url']}}" alt="{{basename($item['low_resolution']['url'])}}"
-             class="img-thumbnail">
-        <figcaption class="figure-caption">
-            <a class="download">
-                <i class="fa fa-download fa-2x" aria-hidden="true"></i>
-            </a>
-        </figcaption>
-    </figure>
-</form>
+<input type="hidden" name="url" value="{{$item['standard_resolution']['url']}}">
+<figure class="figure col-md-4">
+    <img style="height: 400px !important;" src="{{$item['low_resolution']['url']}}"
+         alt="{{basename($item['low_resolution']['url'])}}"
+         class="img-thumbnail">
+    <figcaption class="figure-caption">
+        <a download href="{{$item['low_resolution']['url']}}" class="download">
+            <i class="fa fa-download fa-2x" aria-hidden="true"></i>
+        </a>
+    </figcaption>
+</figure>
--- resources/views/instagram/componant/video.blade.php
+++ resources/views/instagram/componant/video.blade.php
@@ -1,8 +1,5 @@
-<form method="post" action="{{url('download')}}" target="_blank">
-    {{csrf_field()}}
-    <input name="url" type="hidden" value="{{$item['standard_resolution']['url']}}">
+ <input name="url" type="hidden" value="{{$item['standard_resolution']['url']}}">
 <figure class="figure col-md-4">
     <img style="height: 400px !important;" src="{{$item['standard_resolution']['url']}}" class="img-thumbnail" >
-    <figcaption class="figure-caption"><a class="download"  ><i class="fa fa-download fa-2x" aria-hidden="true">video</i></a></figcaption>
+    <figcaption class="figure-caption"><a  download href="{{$item['standard_resolution']['url']}}" class="download"  ><i class="fa fa-download fa-2x" aria-hidden="true">video</i></a></figcaption>
 </figure>
-</form>
