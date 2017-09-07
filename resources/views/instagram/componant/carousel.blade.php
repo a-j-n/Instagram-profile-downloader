4bce98997c07106e68661d3ad6eb297e5377acb9
Ahmed <eng.ahmed_gamal@yahoo.com>
2017-09-07 03:49:16 +0200

    Fix vedio issue

--- resources/views/instagram/componant/carousel.blade.php
+++ resources/views/instagram/componant/carousel.blade.php
@@ -1,3 +1,11 @@
 @foreach($item as $photo)
-    @include('instagram.componant.image',['item'=>$photo['images']])
+    @if($photo['type'] == 'image')
+        @include('instagram.componant.image',['item'=>$photo['images']])
+    @elseif($photo['type'] == 'video')
+        <input name="url" type="hidden" value="{{$photo['videos']['standard_resolution']['url']}}">
+        <figure class="figure col-md-4">
+            <img style="height: 400px !important;" src="{{$photo['videos']['standard_resolution']['url']}}" class="img-thumbnail" >
+            <figcaption class="figure-caption"><a  download href="{{$photo['videos']['standard_resolution']['url']}}" class="download"  ><i class="fa fa-download fa-2x" aria-hidden="true">video</i></a></figcaption>
+        </figure>
+    @endif
 @endforeach
